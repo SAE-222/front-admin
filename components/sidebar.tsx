cfa011d28e363fc0dfa5dee3a919554fb5f8512f
Thiebaut Simon <simon80870@gmail.com>
2023-11-22 17:23:07 +0100

    Sidebar presque finit

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -3,6 +3,9 @@ import { TemplateIcon, ShoppingCartIcon, ShoppingBagIcon, UsersIcon, ChartSquare
 import Link from "next/link"
 import { useState } from "react";
 import { useSelectedLayoutSegment } from "next/navigation";
+import Image from 'next/image'
+import logo from "./LogoNocif.jpg"
+
 
 
 function classNames(...classes: string[]){
@@ -22,10 +25,10 @@ const Sidebar = () => {
 
     return (
         <div className="p-3">
-            <div className="hidden lg:fixed lg:inset-y-0 lg:flex lg:w-72 lg:flex-col">
-                <div className="flex grow flex-col gapy-y-5 overflow-y-auto bg-white px-6 pb-4 border-r-2">
-                    <div className="flex h-16 shrink-0 items-center">
-                        <h1 className="text-3xl font-bold">Logo</h1>
+            <div className="lg:fixed lg:inset-y-0 lg:flex lg:w-72 lg:flex-col">
+                <div className="flex grow flex-col gapy-y-5 overflow-y-auto bg-gray-100 px-6 pb-4 border-r-2">
+                    <div className="flex h-16 shrink-0 items-center ">
+                    <Image src={logo} alt="Large Image" layout="fixed" className="rounded-lg"  objectFit="cover" width={55} height={55}/>
                     </div>
                     <nav className="flex flex-1 flex-col">
                         <ul role="list" className="flex flex-1 flex-col gap-y-7">
@@ -33,9 +36,9 @@ const Sidebar = () => {
                                 <ul role="list" className="-mx-2 space-y-1">
                                     {
                                         sidebarOptions.map((option) =>(
-                                            <li key={option.name}>
-                                                <Link href={option.href} className={classNames(option.current ? "bg-gray-700 text-white" : "text-gray-400 hover:text-white hover:bg-gray-700", "group flex gap-x-3 rounded-md p-2 text-sm leading-6 fontsemibold" )}>
-                                                <option.icon className="text-gray-300 group-hover:text-white h-6 w-6 shrink-0"/>
+                                            <li key={option.name} >
+                                                <Link href={option.href} className={classNames(option.current ? "bg-gray-200 text-white" : "text-gray-700 hover:bg-gray-700", "group flex gap-x-3 rounded-md p-2 text-sm leading-6 fontsemibold hover:bg-gray-200" )}>
+                                                <option.icon className="text-gray-700 group-hover:text-white h-6 w-6 shrink-0   "/>
                                                 {option.name}
                                                 </Link>
                                             </li>
