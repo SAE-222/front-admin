9762be1c96856def582a1dc5c8721215297a7222
Thiebaut Simon <simon80870@gmail.com>
2023-11-23 11:25:01 +0100

    Image relou

--- app/page.js
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div>
-      <h1 className="font-bold text-center p-8 text-light-highlight">
-        Dashboard admin
-      </h1>
-    </div>
-  );
-}
---
+++ app/page.tsx
@@ -0,0 +1,47 @@
+import Link from "next/link";
+import Image from 'next/image'
+import logo from "../components/LogoNocif.jpg"
+
+export default function Login() {
+  return (
+    <div className="relative flex flex-col items-center justify-center min-h-screen overflow-hidden">
+      <div className="w-full p-6 bg-white rounded-md shadow-md lg:max-w-xl flex flex-col items-center"> {/* Ajout de classes pour centrer */}
+        <div className="mb-4">
+          <Image src={logo} alt="Large Image" layout="fixed" className="rounded-lg" objectFit="cover" width={100} height={100} />
+        </div>
+        <form className="w-full mt-6">
+          <div className="mb-4">
+            <label
+              htmlFor="email"
+              className="block text-sm font-semibold text-gray-800"
+            >
+              Email
+            </label>
+            <input
+              type="email"
+              className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-md focus:border-gray-400 focus:ring-gray-300 focus:outline-none focus:ring focus:ring-opacity-40"
+            />
+          </div>
+          <div className="mb-2">
+            <label
+              htmlFor="password"
+              className="block text-sm font-semibold text-gray-800"
+            >
+              Password
+            </label>
+            <input
+              type="password"
+              className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-md focus:border-gray-400 focus:ring-gray-300 focus:outline-none focus:ring focus:ring-opacity-40"
+            />
+          </div>
+
+          <div className="mt-2">
+            <button className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-gray-700 rounded-md hover:bg-gray-600 focus:outline-none focus:bg-gray-600">
+              Login
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
