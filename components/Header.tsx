329300a35f31e1ff50b00bc932461f6502e64544
Thiebaut Simon <simon80870@gmail.com>
2023-11-24 10:34:09 +0100

    Avancées Header

--- components/Header.tsx
+++ components/Header.tsx
@@ -14,39 +14,45 @@ import {
 
 const Header = () => {
   return (
-    <div className="grid grid-cols-3 divide-x">
-        {/* Colonne 1 (vide) */}
-        <div></div>
+    <div className="grid grid-cols-3 gap-9 ">
+      {/* Colonne 1 (vide) */}
+      <div></div>
+      {/* Colonne 2 (vide) */}
+      <div></div>
 
+      {/* Colonne 3 (contenu principal) */}
+      <div className="container bg-gray-200 rounded-bl-[100px]">
+        <header className="relative text-white  ">
+          <div className="flex items-center ml-auto">
+            {/* Barre de recherche */}
+            <SearchBar />
+            
+            {/* Icône de soleil */}
+            <div>
+              <span className="px-4 ml-4">
+                <SunIcon className="w-6 h-6 text-gray-700 cursor-pointer" />
+              </span>
+            </div>
 
+            {/* Logo */}
+            <div className="flex items-center px-4">
+              <Image src={logo} className="rounded-full " alt="Logo" width={40} height={40} />
+            </div>
 
-        {/* Colonne 2 (contenu principal) */}
-        <div className="container mx-auto flex items-center justify-between relative z-10 bg-gray-200 rounded-bl-[100px]">
-            <header className="relative text-white p-4">
-                <div className="flex items-center ml-auto">
-                    {/* Barre de recherche */}
-                    <SearchBar />
-                    <div>
-                        <span className="px-4 ml-4">
-                            <SunIcon className="w-6 h-6 text-gray-700" />
-                        </span>
-                    </div>
-                    <div className="flex items-center px-4">
-                        <Image src={logo} className="rounded-full" alt="Logo" width={40} height={40} />
-                    </div>
-                    <DropdownMenu>
-                    <DropdownMenuTrigger className="flex items-center cursor-pointer">
-                    <ChevronDownIcon className="w-6 h-6 text-gray-700" />
-                    </DropdownMenuTrigger>
-                    <DropdownMenuContent className="text-gray-800">
-                    <DropdownMenuLabel>My Account</DropdownMenuLabel>
-                    <DropdownMenuSeparator />
-                    <DropdownMenuItem>Profile</DropdownMenuItem>
-                    </DropdownMenuContent>
-                    </DropdownMenu>
-                </div>
-            </header>
-        </div>
+            {/* Menu déroulant */}
+            <DropdownMenu>
+              <DropdownMenuTrigger className="flex items-center cursor-pointer">
+                <ChevronDownIcon className="w-6 h-6 text-gray-700" />
+              </DropdownMenuTrigger>
+              <DropdownMenuContent className="text-gray-800">
+                <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem>Profile</DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </div>
+        </header>
+      </div>
     </div>
   );
 };
@@ -62,12 +68,10 @@ const SearchBar = () => {
         aria-label="Search"
       />
       <span className="absolute right-4">
-        <SearchIcon className="w-6 h-6 text-gray-700" />
+        <SearchIcon className="w-6 h-6 text-gray-700 cursor-pointer" />
       </span>
     </div>
   );
 };
 
 export default Header;
-
-
