de895145cbe078fa38c1f977c056a5dea447a36f
Thiebaut Simon <simon80870@gmail.com>
2023-11-30 17:26:58 +0100

    Avanvés AjouterProduit2

--- app/(routes)/dashboard/produits/ajouter/page.tsx
+++ app/(routes)/dashboard/produits/ajouter/page.tsx
@@ -22,6 +22,7 @@ function AjouterProduit() {
       <div>
         <h1 className="font-bold text-3xl">Ajouter Produit</h1>
 
+        <div className='flex space-x-4'>
         <div className=' bg-gray-100 rounded-lg w-2/3'>
         
         <div className='flex'>
@@ -46,10 +47,10 @@ function AjouterProduit() {
 
           <div className='px-2 py-2'>
             <h1>Stock</h1>
-            <div className="bg-white rounded-md w-[158px] h-[42px] border" data-te-input-wrapper-init>
+            <div data-te-input-wrapper-init>
               <input
                 type="number"
-                className="focus:outline-none focus:border-gray-700 text-black peer block min-h-[auto]  bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:peer-focus:text-primary [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
+                className="focus:outline-none bg-white rounded-md w-[158px] h-[42px] border focus:border-gray-700 text-black peer block min-h-[auto]  bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:peer-focus:text-primary [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
                 id="exampleFormControlInputNumber"
                 placeholder="Example label" />
               <label
@@ -118,30 +119,41 @@ function AjouterProduit() {
 
         <div className='  py-2 space-x-2'>
 
-          <div className='flex space-x-2'>
-            <h1 className="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300">Non Disponible</h1>
-            <label className="relative inline-flex items-center mb-5 cursor-pointer">
-              <input type="checkbox" value="" className="sr-only peer"/>
-              <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:w-5 after:h-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
-            </label>
-            <h1 className="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300">Disponible</h1>
-          </div>
+          
 
-          <div className="mb-3  px-2 py-2">
+        </div>
+       
+
+      </div>
+
+      <div className='pt-[248px]'>
+        <div className='space-x-2 bg-gray-100 rounded-lg'>
+
+        <div className="mb-3  px-2 py-2">
             <label
               htmlFor="formFileMultiple"
               className="mb-2 inline-block text-neutral-700 dark:text-neutral-200"
-              >Multiple files input example</label
+              >Images</label
             >
             <input
-              className="relative bg-white m-0 block w-full min-w-0 flex-auto rounded border border-solid border-neutral-300 bg-clip-padding px-3 text-base font-normal text-neutral-700 transition duration-300 ease-in-out file:-mx-3 file:-my-[0.32rem] file:overflow-hidden file:rounded-none file:border-0 file:border-solid file:border-inherit file:bg-neutral-100 file:px-3 file:py-[0.32rem] file:text-neutral-700 file:transition file:duration-150 file:ease-in-out file:[border-inline-end-width:1px] file:[margin-inline-end:0.75rem] hover:file:bg-neutral-200 focus:border-primary focus:text-neutral-700 focus:shadow-te-primary focus:outline-none dark:border-neutral-600 dark:text-neutral-200 dark:file:bg-neutral-700 dark:file:text-neutral-100 dark:focus:border-primary"
+              className="relative bg-white m-0 block w-full min-w-0 rounded border border-solid border-neutral-300 bg-clip-padding px-3 text-base font-normal text-neutral-700 transition duration-300 ease-in-out file:-mx-3 file:-my-[0.32rem] file:overflow-hidden file:rounded-none file:border-0 file:border-solid file:border-inherit file:bg-neutral-100 file:px-3 file:py-[0.32rem] file:text-neutral-700 file:transition file:duration-150 file:ease-in-out file:[border-inline-end-width:1px] file:[margin-inline-end:0.75rem] hover:file:bg-neutral-200 focus:border-primary focus:text-neutral-700 focus:shadow-te-primary focus:outline-none dark:border-neutral-600 dark:text-neutral-200 dark:file:bg-neutral-700 dark:file:text-neutral-100 dark:focus:border-primary"
               type="file"
               id="formFileMultiple"
               multiple />
           </div>
+      <h1>Disponibilité</h1>
+      <div className='flex justify-center pt-2 gap-2 text-sm'>
+            <h1>Non Disponible</h1>
+            <label className="relative inline-flex items-center mb-5 cursor-pointer">
+              <input type="checkbox" value="" className="sr-only peer"/>
+              <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:w-5 after:h-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+            </label>
+            <h1>Disponible</h1>
+          </div>
 
-        </div>
-       
+          
+          </div>
+      </div>
 
       </div>
 
